513fa661df538770405a6fe6d06ce249e639d79e
Not4Zy <519419490@qq.com>
2017-10-29 11:15:09 -0400

    update

--- src/ImageProcessor.java
+++ src/ImageProcessor.java
@@ -14,6 +14,24 @@ public class ImageProcessor {
     }
 
     private void Sobel() {
+        // int[][] mask = { {0, 1, 0}, {1, -4, 1}, {0, 1, 0} };
+        int[][] result = new int[height][width];
+        int i, j;
+        for (i = 1; i < height - 2; i++) {
+            for (j = 1; j < width - 2; j++) {
+                result[i][j] = image[i - 1][j] + image[i][j - 1] +
+                               image[i + 1][j] + image[i][j + 1] - 4 * image[i][j];
+                if (result[i][j] < 0) {
+                    result[i][j] = 0;
+                }
+                if (result[i][j] > 255) {
+                    result[i][j] = 255;
+                }
+            }
+        }
+    }
 
+    private void drawImage(int[][] matrix, String path) {
+        // draw from bufferedImage
     }
 }
